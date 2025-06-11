49ab451c22afe2a63b89845813f0a0b8c4e9e690
Leo <leohuari2406@hotmail.com>
2025-06-10 21:41:41 -0300

    Se agrego la clase Heap

---
+++ src/main/java/Heaps/Heap.java
@@ -0,0 +1,94 @@
+package Heaps;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Heap<T extends Comparable<T>> {
+
+    private ArrayList<T> cola;
+
+    public Heap(){
+        this.cola = new ArrayList<>();
+    }
+
+    public Heap(T[] array){
+        this.cola = new ArrayList<>(Arrays.asList(array));
+        for (int i = this.cola.size() / 2 - 1; i >= 0; i--) {
+            this.bajar(i);
+        }
+    }
+
+    public void agregar(T elem){
+        if (this.cola.size()==0) {
+            this.cola.add(elem);
+        }else{
+            this.cola.add(elem);
+            int indice = this.cola.size()-1;
+            subir(indice);
+        }
+    }
+
+    private void subir(int index){
+        int indexPadre = (index - 1) / 2;
+        T hijo = this.cola.get(index);
+        T padre = this.cola.get(indexPadre);
+        while (index > 0 && hijo.compareTo(padre) > 0) {
+            this.cola.set(index, padre);
+            this.cola.set(indexPadre, hijo);
+            index = indexPadre;
+            indexPadre = (index - 1) / 2;
+        }
+    }
+
+    private void bajar(int index){
+        int hijoIzq = 2 * index + 1;
+        int hijoDer = 2 * index + 2;
+        int mayor = index;
+
+        if (hijoIzq < this.cola.size() && this.cola.get(hijoIzq).compareTo(this.cola.get(mayor)) > 0) {
+            mayor = hijoIzq;
+        }
+        if (hijoDer < this.cola.size() && this.cola.get(hijoDer).compareTo(this.cola.get(mayor)) > 0) {
+            mayor = hijoDer;
+        }
+    
+        if (mayor != index) {
+            T temp = this.cola.get(index);
+            this.cola.set(index, this.cola.get(mayor));
+            this.cola.set(mayor, temp);
+            bajar(mayor);
+        }
+    }
+
+    public T extraer(){
+        T res = this.cola.get(0);
+        this.cola.set(0, this.cola.get(this.cola.size() - 1));
+        this.cola.remove(this.cola.size() - 1);
+        this.bajar(0);
+        return res;
+    }
+    
+    public T ver(){
+        return this.cola.get(0);
+    }
+
+    public int cardinalidad(){
+        return this.cola.size();
+    }
+
+    public boolean contiene(T elem){
+        return this.cola.contains(elem);
+    }
+
+    public boolean esVacio(){
+        return this.cola.isEmpty();
+    }
+
+    public void vaciar() {
+        this.cola.clear();
+    }
+
+    public ArrayList<T> getHeap(){
+        return this.cola;
+    }
+}
--- src/main/java/aed/main.java
+++ src/main/java/aed/main.java
@@ -1,18 +1,24 @@
 package aed;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import Heaps.Heap;
 import ListaDE.*;
 
 public class main {
     public static void main(String[] args) {
-        int n = 15;
-        Nodo<Integer> nodo = new Nodo<Integer>(n);
-        int a = 20;
-        Nodo<Integer> nodo2 = new Nodo<Integer>(a);
-        Nodo<Integer> nodo3 =null;
-        System.out.println(nodo.getAnterior());
-        System.out.println(nodo.getSiguiente());
-        nodo.setAnterior(nodo2);
-        System.out.println(nodo.getAnterior().valor());
-        System.out.println(nodo3.valor());
+        Integer[] n = {1,5,7,1,3,5,4};
+        Heap<Integer> h = new Heap<>(n);
+
+        System.out.println(h.getHeap());
+        System.out.println(h.ver());
+        h.extraer();
+        System.out.println(h.ver());
+        System.out.println(h.getHeap());
+        Integer num = 9;
+        h.agregar(num);
+        System.out.println(h.getHeap());
     }
 }
